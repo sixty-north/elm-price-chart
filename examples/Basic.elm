293db391ef00e69d839d7ee1d201a4030eeeeed1
Austin Bingham <austin@sixty-north.com>
2017-06-04 20:20:12 +0200

    Using proper data in Basic.elm example.

--- examples/Basic.elm
+++ examples/Basic.elm
@@ -1,9 +1,13 @@
 module Basic exposing (..)
 
+import Date
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import PriceChart
 import PriceChart.Types
 import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Tesla
 
 
 type Msg
@@ -16,7 +20,13 @@ type alias Model =
 
 initialModel : Model
 initialModel =
-    { priceChart = PriceChart.initialModel }
+    let
+        pcm = PriceChart.initialModel
+        date = List.reverse Tesla.tesla |> List.head |> Maybe.andThen (.startDate >> Just) |> Maybe.withDefault (Date.fromTime 0)
+        x = Debug.log "prices: " Tesla.tesla
+        y = Debug.log "date: " date
+    in
+        { priceChart = {pcm | prices = Tesla.tesla, startDate = date}}
 
 
 update : Msg -> Model -> ( Model, Cmd.Cmd Msg )
@@ -29,16 +39,15 @@ update msg model =
             in
                 { model | priceChart = mdl } ! [ Cmd.map PriceChartMsg cmd ]
 
-
 view : Model -> Html.Html Msg
 view model =
     let
         -- This is a guess and almost certainly wrong!!!
         screenRect =
-            PriceChart.Types.ElementRect 0 1000 0 300
+            PriceChart.Types.ElementRect 0 1000 0 400
     in
-        div []
-            [ svg [] [ PriceChart.priceChart model.priceChart screenRect |> Html.map PriceChartMsg ] ]
+        div [Html.Attributes.width 1000, Html.Attributes.height 400]
+            [ svg [viewBox "0 0 1000 400" ] [ PriceChart.priceChart model.priceChart screenRect |> Html.map PriceChartMsg ] ]
 
 
 main =
